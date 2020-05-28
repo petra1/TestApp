6ccb2939c67138d55ac9bcc8bddc57008794fff2
petra1 <petra@access-for-all.ch>
2020-05-28 12:33:51 +0200

    Try to include a WebView for a  local html file but it does not display.

--- app/src/main/java/ch/petraaccess_for_all/testapp/MainActivity.java
+++ app/src/main/java/ch/petraaccess_for_all/testapp/MainActivity.java
@@ -1,11 +1,9 @@
 package ch.petraaccess_for_all.testapp;
 
 import android.os.Bundle;
-import android.view.View;
+//import android.view.View;
 import android.view.Menu;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -39,6 +37,8 @@ public class    MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController (this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController (this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController (navigationView, navController);
+
+
     }
 
     @Override
--- app/src/main/java/ch/petraaccess_for_all/testapp/WebViewFragment.java
+++ app/src/main/java/ch/petraaccess_for_all/testapp/WebViewFragment.java
@@ -7,9 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 
-/**
+/*
  * A simple {@link Fragment} subclass.
  * Use the {@link WebViewFragment#newInstance} factory method to
  * create an instance of this fragment.
@@ -28,25 +29,7 @@ public class WebViewFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WebViewFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WebViewFragment newInstance(String param1, String param2) {
-        WebViewFragment fragment = new WebViewFragment ();
-        Bundle args = new Bundle ();
-        args.putString (ARG_PARAM1, param1);
-        args.putString (ARG_PARAM2, param2);
-        fragment.setArguments (args);
-        return fragment;
-    }
-
-    @Override
+     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         if (getArguments () != null) {
@@ -58,7 +41,14 @@ public class WebViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View v = inflater.inflate (R.layout.fragment_web_view, container, false);
+        WebView webView = (WebView) v.findViewById (R.id.webView);
+        webView.loadUrl ("file:///android_asset/index.html");
+
+
         // Inflate the layout for this fragment
         return inflater.inflate (R.layout.fragment_web_view, container, false);
+
     }
 }
